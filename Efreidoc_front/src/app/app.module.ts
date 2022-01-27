2b0afdaedb608eeeda6c317a191ed4852bc0d761
Jamesjin18 <maman1000000@hotmail.fr>
2022-01-27 18:20:17 +0100

    add login html css

--- Efreidoc_front/src/app/app.module.ts
+++ Efreidoc_front/src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './modules/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SignupComponent } from './modules/signup/signup.component';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +18,10 @@ import { SignupComponent } from './modules/signup/signup.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
