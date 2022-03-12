c49f97f4a95cb3cf649cb623f19287a1a3e5d9f1
Jamesjin18 <maman1000000@hotmail.fr>
2022-03-12 22:29:56 +0100

    change Item to majeur

--- Efreidoc_front/src/app/modules/selecmat/selecmat.component.ts
+++ Efreidoc_front/src/app/modules/selecmat/selecmat.component.ts
@@ -2,14 +2,14 @@ import { Component, Input, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, QuerySnapshot } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { MajeurService } from 'src/app/core/services/majeur.service';
-import { Item } from 'src/app/models/Item';
+import { Majeur } from 'src/app/models/majeur';
 @Component({
   selector: 'app-selecmat',
   templateUrl: './selecmat.component.html',
   styleUrls: ['./selecmat.component.css']
 })
 export class SelecmatComponent implements OnInit {
-  majeurList: Item[] | undefined;
+  majeurList: Majeur[] | undefined;
 
   constructor(afs:AngularFirestore, private majeurService: MajeurService) {
     this.majeurService.getMajeurs().subscribe(items => {
