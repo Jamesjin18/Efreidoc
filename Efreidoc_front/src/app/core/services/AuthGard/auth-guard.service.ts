7b207b2f6202ce267dab2067ac07f2bf6900ee60
Fyreti <etienne.ygorra@efrei.net>
2022-03-16 15:25:04 +0100

    login auth guard

---
+++ Efreidoc_front/src/app/core/services/AuthGard/auth-guard.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { CanActivate, Router } from '@angular/router';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, public afAuth: AngularFireAuth) {}
+
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    return new Promise((resolve, reject) => {
+      this.afAuth.onAuthStateChanged((user) => {
+        if (user) {
+          if (user.emailVerified) {
+            console.log('email verified');
+            resolve(true);
+          } else {
+            console.log('email not verified');
+            this.router.navigate(['/login']);
+            reject(false);
+          }
+        } else {
+          console.log('no user');
+          this.router.navigate(['/login']);
+          reject(false);
+        }
+      });
+    });
+  }
+}
