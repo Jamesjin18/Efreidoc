4022bfe61c64f507232dd189978bd6a327d605cd
Jamesjin18 <maman1000000@hotmail.fr>
2022-03-16 20:14:18 +0100

    add cours type

---
+++ Efreidoc_front/src/app/select-cours-type/select-cours-type.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SelectCoursTypeComponent } from './select-cours-type.component';
+
+describe('SelectCoursTypeComponent', () => {
+  let component: SelectCoursTypeComponent;
+  let fixture: ComponentFixture<SelectCoursTypeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SelectCoursTypeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SelectCoursTypeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
