0d70a3812c24a6681213cd156c95da84f460438d
Fyreti <etienne.ygorra@efrei.net>
2022-03-16 20:24:04 +0100

    add cours type

--- Efreidoc_front/src/app/select-cours-type/select-cours-type.component.ts
+++ Efreidoc_front/src/app/select-cours-type/select-cours-type.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-select-cours-type',
   templateUrl: './select-cours-type.component.html',
-  styleUrls: ['./select-cours-type.component.css']
+  styleUrls: ['./select-cours-type.component.css'],
 })
 export class SelectCoursTypeComponent implements OnInit {
-
   selectedPromo!: string;
   selectedClass!: string;
   selectedCours!: string;
@@ -16,7 +17,8 @@ export class SelectCoursTypeComponent implements OnInit {
   coursTypeSnap: any;
   constructor(
     private router: ActivatedRoute,
-    private afs: AngularFirestore
+    private afs: AngularFirestore,
+    public appComponent: AppComponent
   ) {}
 
   ngOnInit(): void {
@@ -26,9 +28,49 @@ export class SelectCoursTypeComponent implements OnInit {
       this.selectedCours = params['cours'];
     });
     this.afs
-      .collection('efrei/' + this.selectedPromo + '/class/'+this.selectedClass+'/cours/'+this.selectedCours+'/coursType')
+      .collection(
+        'efrei/' +
+          this.selectedPromo +
+          '/class/' +
+          this.selectedClass +
+          '/cours/' +
+          this.selectedCours +
+          '/coursType'
+      )
       .ref.get()
       .then((data) => (this.coursTypeSnap = data.docs));
   }
 
+  openAddCoursType() {
+    Swal.fire({
+      title: 'Nom de la matière',
+      input: 'text',
+      inputAttributes: {
+        autocapitalize: 'off',
+      },
+      showCancelButton: true,
+      confirmButtonText: 'ajouter',
+      showLoaderOnConfirm: true,
+      allowOutsideClick: () => !Swal.isLoading(),
+    }).then((result) => {
+      if (result.isConfirmed) {
+        if (result.value) {
+          this.addCoursType(result.value);
+        }
+      }
+    });
+  }
+
+  addCoursType(nameCoursType: string) {
+    this.afs
+      .collection('efrei')
+      .doc(this.selectedPromo)
+      .collection('class')
+      .doc(this.selectedClass)
+      .collection('cours')
+      .doc(this.selectedCours)
+      .collection('coursType')
+      .doc(nameCoursType)
+      .set({}, { merge: true });
+  }
 }
