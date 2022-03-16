fba3ef066a0a6d0a629b8b60a604043df48b72e1
Fyreti <etienne.ygorra@efrei.net>
2022-03-16 22:14:19 +0100

    upload

--- Efreidoc_front/src/app/select-documents/select-documents.component.ts
+++ Efreidoc_front/src/app/select-documents/select-documents.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
 import { ActivatedRoute } from '@angular/router';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-select-documents',
@@ -13,11 +17,11 @@ export class SelectDocumentsComponent implements OnInit {
   selectedCours!: string;
   selectedCoursType!: string;
 
+  public progress = 0;
+  public folderinit = '';
+
   documentsSnap: any;
-  constructor(
-    private router: ActivatedRoute,
-    private afs: AngularFirestore
-  ) {}
+  constructor(private router: ActivatedRoute, private afs: AngularFirestore) {}
 
   ngOnInit(): void {
     this.router.params.subscribe((params) => {
@@ -25,15 +29,174 @@ export class SelectDocumentsComponent implements OnInit {
       this.selectedClass = params['class'];
       this.selectedCours = params['cours'];
       this.selectedCoursType = params['coursType'];
+      this.folderinit =
+        this.selectedPromo +
+        '/' +
+        this.selectedClass +
+        '/' +
+        this.selectedCours +
+        '/' +
+        this.selectedCoursType;
     });
     this.afs
-      .collection('efrei/' +this.selectedPromo + 
-      '/class/'+this.selectedClass+
-      '/cours/'+this.selectedCours+
-      '/coursType/'+this.selectedCoursType
-      + '/documents')
+      .collection(
+        'efrei/' +
+          this.selectedPromo +
+          '/class/' +
+          this.selectedClass +
+          '/cours/' +
+          this.selectedCours +
+          '/coursType/' +
+          this.selectedCoursType +
+          '/documents'
+      )
       .ref.get()
       .then((data) => (this.documentsSnap = data.docs));
-    console.log(this.documentsSnap)
+    console.log(this.documentsSnap);
+  }
+
+  uploading(event: any) {
+    console.log(event);
+    var allFile = event.target.files;
+    this.submiteUploadFormPictures(allFile);
+  }
+
+  submiteUploadFormPictures(allFile: Array<any>): void {
+    console.log(allFile);
+    const date = new Date();
+    const dateUpload = date.getTime();
+    for (let i in allFile) {
+      // Create a root reference
+      const storageRef = firebase.storage().ref();
+
+      // Create the file metadata
+      const metadata = {
+        contentType: 'image/jpeg/gif/png/txt',
+      };
+
+      // Upload file and metadata to the object 'images/mountains.jpg'
+      if (allFile[i].webkitRelativePath) {
+        var filePath = allFile[i].webkitRelativePath.slice(
+          0,
+          allFile[i].webkitRelativePath.lastIndexOf('/')
+        );
+      }
+      //var filePath = allFile[0].webkitRelativePath.slice(0,allFile[0].webkitRelativePath.lastIndexOf('/'))
+      console.log(allFile[i].webkitRelativePath);
+      console.log(filePath);
+      const uploadTask = storageRef
+        .child(
+          'efrei/' +
+            this.folderinit +
+            '/' +
+            dateUpload +
+            '/' +
+            filePath +
+            '/' +
+            allFile[i].name
+        )
+        .put(allFile[i], metadata);
+
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(
+        firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        (snapshot: any) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          this.progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + this.progress + '% done');
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+              console.log('Upload is paused');
+              break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+              console.log('Upload is running');
+              break;
+          }
+        },
+        (error: any) => {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case 'storage/unauthorized':
+              // User doesn't have permission to access the object
+              break;
+            case 'storage/canceled':
+              // User canceled the upload
+              break;
+
+            // ...
+
+            case 'storage/unknown':
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+          }
+        },
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL: any) => {
+            console.log('File available at', downloadURL);
+            const ref = this.afs
+              .collection('efrei')
+              .doc(this.selectedPromo)
+              .collection('class')
+              .doc(this.selectedClass)
+              .collection('cours')
+              .doc(this.selectedCours)
+              .collection('coursType')
+              .doc(this.selectedCoursType)
+              .collection('documents')
+              .doc(dateUpload.toString());
+
+            ref
+              .set(
+                { path: this.folderinit + '/' + dateUpload.toString() },
+                { merge: true }
+              )
+              .then(() => {
+                for (let file of allFile) {
+                  this.setUploadName(
+                    ref,
+                    file.webkitRelativePath,
+                    dateUpload.toString(),
+                    uploadTask
+                  );
+                }
+              });
+          });
+        }
+      );
+    }
+  }
+
+  setUploadName(
+    ref: AngularFirestoreDocument<firebase.firestore.DocumentData>,
+    filePath: string,
+    date: string,
+    uploadTask: firebase.storage.UploadTask
+  ) {
+    uploadTask.snapshot.ref.getMetadata().then((metadata) => {
+      const allNameFolder: string[] = filePath.split('/');
+      let refUpdate = ref;
+      let index = 0;
+      let path = this.folderinit + '/' + date;
+      for (let nameFolder of allNameFolder) {
+        path = path + '/' + nameFolder;
+        if (index < allNameFolder.length - 1) {
+          refUpdate
+            .collection('folder')
+            .doc(nameFolder)
+            .set({ path: path }, { merge: true });
+          refUpdate = refUpdate.collection('folder').doc(nameFolder);
+          index++;
+        } else {
+          refUpdate
+            .collection('file')
+            .doc(nameFolder)
+            .set({ path: path, size: metadata.size }, { merge: true });
+          refUpdate = refUpdate.collection('folder').doc(nameFolder);
+        }
+      }
+    });
   }
 }
