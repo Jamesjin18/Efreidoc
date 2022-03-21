f5d1b7512dcac0205550540d4b7c9e04968b38f9
Jamesjin18 <maman1000000@hotmail.fr>
2022-03-21 01:32:48 +0100

    add profil component

--- src/app/modules/compte/compte.component.ts
+++ src/app/modules/compte/compte.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AppComponent } from 'src/app/app.component';
+import { TableService } from 'src/app/core/services/table.service';
 
 @Component({
   selector: 'app-compte',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compte.component.css']
 })
 export class CompteComponent implements OnInit {
-
-  constructor() { }
+  documentsSnap: any;
+  constructor(private afs:AngularFirestore, private afAuth:AngularFireAuth,private tableService:TableService,public appComponent:AppComponent ) {
+    
+  }
 
   ngOnInit(): void {
+    this.afAuth.currentUser.then((user)=>{
+      this.afs
+      .collection('users').doc(user!.uid).collection('upload')
+      .ref.get()
+      .then((data) => (this.documentsSnap = data.docs));
+    })
+  }
+  
+  async pageTokenExample(folder: string, folderinit: string) {
+    this.tableService.pageTokenExample(folder,folderinit)
+  }
+
+  popUpDescriptionSize(desc: string) {
+    this.tableService.popUpDescriptionSize(desc);
   }
 
 }
