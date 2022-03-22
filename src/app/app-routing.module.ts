9807fbb3b0913d4865dd7cdca53c75af67986723
Jamesjin18 <maman1000000@hotmail.fr>
2022-03-22 22:53:41 +0100

    make breadcrumb prettier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -55,8 +55,8 @@ const routes: Routes = [
     component: ResourcespageComponent,
   },
   { canActivate: [AuthGuard], path: 'ajoutdoc', component: AjoutdocComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
 ];
 
 @NgModule({
